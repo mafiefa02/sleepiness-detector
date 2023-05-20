22f01a0bdae5323007c1e1df85d214e001a5657c
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-05-20 12:44:28 +0700

    refactor

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import numpy as np
 import cv2
 from cvzone.FaceMeshModule import FaceMeshDetector
-from variables import faceFeaturePos, color
+from variables import *
 from cameraPorts import chooseCamera
 
 
 def main():
     video = cv2.VideoCapture(chooseCamera())
     detector = FaceMeshDetector(maxFaces=1)
-    # face_cascade = cv2.CascadeClassifier(
-    # cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    # eye_cascade = cv2.CascadeClassifier(
-    # cv2.data.haarcascades + 'haarcascade_eye.xml')
 
     while True:
         ret, frame = video.read()
@@ -21,26 +17,29 @@ def main():
         if faces:
             face = faces[0]
 
-            # up, down, left, right
-            leftEye = [faceFeaturePos["upLeftEye"],
-                       faceFeaturePos["downLeftEye"],
-                       faceFeaturePos["leftLeftEye"],
-                       faceFeaturePos["rightLeftEye"]]
-
-            rightEye = [faceFeaturePos["upRightEye"],
-                        faceFeaturePos["downLeftEye"],
-                        faceFeaturePos["leftRightEye"],
-                        faceFeaturePos["rightRightEye"]]
-
-            mouth = [faceFeaturePos["upMouth"],
-                     faceFeaturePos["downMouth"],
-                     faceFeaturePos["leftMouth"],
-                     faceFeaturePos["rightMouth"]]
-
-            faceId = leftEye + rightEye + mouth
-
             for id in faceId:
-                cv2.circle(frame, face[id], 5, color["blue"], cv2.FILLED)
+                cv2.circle(frame, face[id], 3, color["red"], cv2.FILLED)
+
+            leftEyeVerticalDistance, leftEyeVerticalPos = detector.findDistance(
+                face[upLeftEye], face[downLeftEye])
+            rightEyeVerticalDistance, rightEyeVerticalPos = detector.findDistance(
+                face[upRightEye], face[downRightEye])
+            leftEyeHorizontalDistance, leftEyeHorizontalPos = detector.findDistance(
+                face[leftLeftEye], face[rightLeftEye])
+            rightEyeHorizontalDistance, rightEyeHorizontalPos = detector.findDistance(
+                face[leftRightEye], face[rightRightEye])
+
+            leftEyeRatio = 100*(leftEyeVerticalDistance /
+                                leftEyeHorizontalDistance)
+            rightEyeRatio = 100*(rightEyeVerticalDistance /
+                                 rightEyeHorizontalDistance)
+
+            mouthVerticalDistance, mouthVerticalPos = detector.findDistance(
+                face[upMouth], face[downMouth])
+            mouthHorizontalDistance, mouthHorizontalPos = detector.findDistance(
+                face[leftMouth], face[rightMouth])
+
+            mouthRatio = 100*(mouthVerticalDistance/mouthHorizontalDistance)
 
         cv2.imshow('video capture', frame)
 
--- variables.py
+++ variables.py
@@ -1,17 +1,20 @@
-faceFeaturePos = {
-    "upLeftEye": 27,
-    "downLeftEye": 23,
-    "leftLeftEye": 130,
-    "rightLeftEye": 243,
-    "upRightEye": 257,
-    "downRightEye": 253,
-    "leftRightEye": 463,
-    "rightRightEye": 359,
-    "upMouth": 11,
-    "downMouth": 16,
-    "leftMouth": 57,
-    "rightMouth": 287
-}
+upLeftEye = 27
+downLeftEye = 23
+leftLeftEye = 130
+rightLeftEye = 243
+upRightEye = 257
+downRightEye = 253
+leftRightEye = 463
+rightRightEye = 359
+upMouth = 11
+downMouth = 16
+leftMouth = 57
+rightMouth = 287
+
+leftEye = [upLeftEye, downLeftEye, leftLeftEye, rightLeftEye]
+rightEye = [upRightEye, downRightEye, leftRightEye, rightRightEye]
+mouth = [upMouth, downMouth, leftMouth, rightMouth]
+faceId = leftEye + rightEye + mouth
 
 color = {
     "red": (0, 0, 255),
